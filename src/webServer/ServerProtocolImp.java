464d21ff82400e06c11f2e65ad864bbd11a2b3cd
noamara <noamara@c9418416-3347-11de-9e80-b52b74bdfc52>
2009-05-20 16:26:33 +0000

    All the regular user functions are implemented and tested. The search functions are not implemented yet

--- src/webServer/ServerProtocolImp.java
+++ src/webServer/ServerProtocolImp.java
@@ -28,90 +28,185 @@ public class ServerProtocolImp implements ServerProtocol {
 	}
 
 	/**
-	 * NOT YET IMPLEMENTED
+	 * This is the method that implements the basic protocol between the
+	 * user and the server. This method receives a request from the client
+	 * parses it and returns an answer in form of a string "command args"
+	 * IMPORTANT: every response has to end with '\\e'
 	 */
 	@Override
 	public String processMessage(String msg) {
 		String parsedString[] = msg.split(" ");
 		if (parsedString[0].contentEquals("login")){
-			try {
-				_connectedUser = _controller.logMeIn(parsedString[1], parsedString[2]);
-				if (_connectedUser != null)
-				{
-					return "print user logged in succesfully \\e";
-				}
-				else
-				{
-					return "print username and/or password are incorrect \\e";
-				}
-			} catch (UserDoesNotExistException e) {
-				return "print username and/or password are incorrect \\e";
-			}
+			return userLogIn(parsedString);
 		}
 		if (parsedString[0].contentEquals("register"))
 		{
-			try {
-				if (_controller.registerNewUser(parsedString[1], parsedString[2]))
-				{
-					return "print Registration successfull \\e";
-				}
-			} catch (UserAlreadyExistsException e) {
-				return "print unable was not successful \\e";
-			}
+			return registerNewUser(parsedString);
 		}
 		if (parsedString[0].contentEquals("message"))
 		{
-			if (_connectedUser == null)
-			{
-				return "print you are not logged in \\e";
-			}
-			String messageContent = msg.substring(parsedString[0].length());
-			_controller.addNewMessage(new MessageDataImp(messageContent), _connectedUser);
-			return "print message added \\e";
+			return addNewMessage(msg, parsedString);
 		}
 		if (parsedString[0].contentEquals("reply"))
 		{
-			if (_connectedUser == null)
-			{
-				return "print you are not logged in \\e";
-			}
-			String msgFather = parsedString[parsedString.length-1];
-			String messageContent = msg.substring(parsedString[0].length());
-			messageContent.substring(0, messageContent.indexOf(msgFather)-1);
-			_controller.replyToMessage(new MessageDataImp(messageContent), _connectedUser, Long.parseLong(msgFather));
-			return "print message added \\e";
+			return replyToMessage(msg, parsedString);
 		}
-
+		if (parsedString[0].contentEquals("edit")){
+			return editMessage(msg, parsedString);
+		}
 		if (parsedString[0].contentEquals("display"))
 		{
-			Collection<Message> allMessages;
-			allMessages = _controller.getAllMessagesChildren(Long.parseLong(parsedString[1]));
-			String returnString = "print ";
-			if (allMessages.isEmpty()){
-				returnString += "No messages found";
-				return returnString + "  \\e";
-			}
-			else
-			{
-				Iterator<Message> it = allMessages.iterator();
-				while (it.hasNext()){
-					Message currentMsg = it.next();
-					returnString += currentMsg.get_mID() + ": " + currentMsg.get_msgBody().displayMessageData() + "\n";
-				}
-				return returnString + " \\e";
-			}
+			return displayMessagesOfFather(parsedString);
 		}
 		if (parsedString[0].contentEquals("logoff"))
 		{
-			if (_connectedUser == null)
-			{
-				return "print you are not logged in \\e";
-			}
-			_controller.logMeOut(_connectedUser);
-			_connectedUser = null;
-			return "print user logged out successfully \\e";
+			return logoffUser();
+		}
+		if (parsedString[0].contentEquals("moderator"))
+		{
+			return giveModeratorPrivellages(parsedString);
+		}
+		if (parsedString[0].contentEquals("member"))
+		{
+			return assignMember(parsedString);
+		}
+		if (parsedString[0].contentEquals("delete"))
+		{
+			return deleteMessage(parsedString);
 		}
 		return "print unknown command \\e";
+	}
+
+	private String deleteMessage(String[] parsedString) {
+		long mid = Long.parseLong(parsedString[1]);
+		if (_controller.deleteMessage(_connectedUser, mid))
+		{
+			return "print message deleted \\e";
+		}
+		else
+		{
+			return "print you are not authorized to delete message. \\e";
+		}
+	}
+
+	private String assignMember(String[] parsedString) {
+		String userName = parsedString[1];
+		RegisteredUser asignee = _controller.getUser(userName);
+		if (_controller.assignMember(_connectedUser, asignee))
+		{
+			return "print user assigned successfully \\e";
+		}
+		else
+		{
+			return "print you are not authorized to asign that user \\e";
+		}
+	}
+
+	private String giveModeratorPrivellages(String[] parsedString) {
+		String userName = parsedString[1];
+		RegisteredUser assignee = _controller.getUser(userName);
+		if (_controller.assignModerator(_connectedUser, assignee))
+		{
+			return "print user was granted moderator privellages \\e";
+		}
+		else
+		{
+			return "print you are not authorized to make that change \\e";
+		}
+	}
+
+	private String logoffUser() {
+		if (_connectedUser == null)
+		{
+			return "print you are not logged in \\e";
+		}
+		_controller.logMeOut(_connectedUser);
+		_connectedUser = null;
+		return "print user logged out successfully \\e";
+	}
+
+	private String displayMessagesOfFather(String[] parsedString) {
+		Collection<Message> allMessages;
+		allMessages = _controller.getAllMessagesChildren(Long.parseLong(parsedString[1]));
+		String returnString = "print ";
+		if (allMessages.isEmpty()){
+			returnString += "No messages found";
+			return returnString + "  \\e";
+		}
+		else
+		{
+			Iterator<Message> it = allMessages.iterator();
+			while (it.hasNext()){
+				Message currentMsg = it.next();
+				returnString += currentMsg.get_mID() + ": " + currentMsg.get_msgBody().displayMessageData() + "\n";
+			}
+			return returnString + " \\e";
+		}
+	}
+
+	private String editMessage(String msg, String[] parsedString) {
+		if (_connectedUser == null)
+		{
+			return "print you are not logged in \\e";
+		}
+		String msgId = parsedString[parsedString.length-1];
+		String messageContent = msg.substring(parsedString[0].length());
+		messageContent = messageContent.substring(0, messageContent.lastIndexOf(msgId));
+		if (_controller.editMsg(_connectedUser, Long.parseLong(msgId), new MessageDataImp(messageContent)))
+			return "print message edited successfully \\e";
+		else
+			return "print you are not authorized to edit the message \\e";
+	}
+	
+	private String replyToMessage(String msg, String[] parsedString) {
+		if (_connectedUser == null)
+		{
+			return "print you are not logged in \\e";
+		}
+		String msgFather = parsedString[parsedString.length-1];
+		String messageContent = msg.substring(parsedString[0].length());
+		messageContent = messageContent.substring(0, messageContent.lastIndexOf(msgFather));
+		_controller.replyToMessage(new MessageDataImp(messageContent), _connectedUser, Long.parseLong(msgFather));
+		return "print message added \\e";
+	}
+
+	private String addNewMessage(String msg, String[] parsedString) {
+		if (_connectedUser == null)
+		{
+			return "print you are not logged in \\e";
+		}
+		String messageContent = msg.substring(parsedString[0].length());
+		_controller.addNewMessage(new MessageDataImp(messageContent), _connectedUser);
+		return "print message added \\e";
+	}
+
+	private String registerNewUser(String[] parsedString) {
+		try {
+			if (_controller.registerNewUser(parsedString[1], parsedString[2]))
+			{
+				return "print Registration successfull \\e";
+			}
+		} catch (UserAlreadyExistsException e) {
+			return "print unable was not successful \\e";
+		}
+		return "print unknown command";
+
+	}
+
+	private String userLogIn(String[] parsedString) {
+		try {
+			_connectedUser = _controller.logMeIn(parsedString[1], parsedString[2]);
+			if (_connectedUser != null)
+			{
+				return "print user logged in succesfully \\e";
+			}
+			else
+			{
+				return "print username and/or password are incorrect \\e";
+			}
+		} catch (UserDoesNotExistException e) {
+			return "print username and/or password are incorrect \\e";
+		}
 	}
 
 }
